5b68a6a03a0b5ba9090ee3ed7e9236ed2b428b23
Shihab Kandil <jupitar.orbit01@gmail.com>
2022-10-22 20:57:43 +0200

    Added SharedPreferences for local user cache

---
+++ lib/data/repository/user_data_cache.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../utils/constants/cache_keys.dart';
+import '../models/user.dart';
+
+class UserDataCache{
+  static late SharedPreferences _localUserData;
+
+  static Future init() async{
+    _localUserData = await SharedPreferences.getInstance();
+}
+
+  void writeUserDataCachePreferences(User user) async {
+    _localUserData.setString(userCacheKey, jsonEncode(user.toJson()));
+  }
+
+  User readUserDataCachePreferences() {
+      Map<String,dynamic> userModel = jsonDecode(_localUserData.getString(userCacheKey)??'');
+      User user = User.fromJson(userModel);
+    print(user);
+    return user;
+  }
+}
