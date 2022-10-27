dc8417e78ae42a3f508d478798292ae39a6e2e91
Bilal Ghareeb <bilal.ghareeb@gmail.com>
2022-10-27 14:50:22 +0200

    Create Profile Page Route

--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:mobile_app_project/screens/main_screens/home_screen/home_screen.dart';
+import 'package:mobile_app_project/screens/main_screens/profile_screen/profile_screen.dart';
 import '../screens/auth_screens/login_screen.dart';
 import '../screens/auth_screens/register_screen.dart';
 /// To navigate use
@@ -8,6 +9,7 @@ import '../screens/auth_screens/register_screen.dart';
 /// onTap: () => context.go('/page_path')
 class AppRouter {
   final GoRouter router = GoRouter(
+       initialLocation:"/home" ,
 
         routes: [
           GoRoute(
@@ -16,10 +18,20 @@ class AppRouter {
           ),
 
            GoRoute(
+            
              path: '/home',
              builder: (context, state) => const HomeScreen(),
+             routes: [
+
+              GoRoute(
+                path: 'profile',
+                builder: (context, state) => const ProfileScreen(),
+              )
+
+             ]
            ),
 
+          
         ],
       // errorBuilder: (context, state) => ErrorScreen(state.error),
     );
