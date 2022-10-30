af681578460f3d2202bbdf030638748ced7de27e
Bilal Ghareeb <bilal.ghareeb@gmail.com>
2022-10-30 22:14:44 +0200

    Adding a Drawer to the Home Screen

---
+++ lib/screens/home_screen/widgets/tile_template.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TileTemplate extends StatelessWidget {
+  const TileTemplate({super.key , required this.icon , required this.callback , required this.text , required this.tail , required this.color});
+
+  final Icon icon,tail;
+  final VoidCallback callback;
+  final Text text;
+  final Color color;
+  
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                 Radius.circular(20)
+              )
+            ), 
+
+            tileColor: color,
+            leading:icon,
+            trailing: tail,
+            title: text,
+            onTap: callback,
+          );
+  }
+}
