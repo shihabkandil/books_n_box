bd85f62274a878bdc1dc768538d80177eac5749c
Shihab Kandil <jupitar.orbit01@gmail.com>
2022-12-05 17:44:36 +0200

    Viewing BestSellerBooks in home_carousel.dart

--- lib/screens/home_screen/widgets/home_carousel.dart
+++ lib/screens/home_screen/widgets/home_carousel.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_app_project/data/models/nytimes_models/nytimes_best_sellers_models/nyt_best_sellers.dart';
 import '../../../business_logic/cubit/nyt_best_sellers_cubit/nyt_best_sellers_cubit.dart';
 import '../../../utils/enums/books_data_status_enum.dart';
 import '../../widgets/book_card.dart';
@@ -22,7 +23,10 @@ class HomeScreenCarousel extends StatelessWidget {
           else if (state.status == BooksDataStatus.booksLoaded && state.books != null) {
             return CarouselSlider.builder(
               itemCount: state.books!.length,
-              itemBuilder: (BuildContext context, _, __) => BookCard(),
+              itemBuilder: (BuildContext context, bookIndex, __) {
+                BestSellerBook bestSellerBook = state.books!.elementAt(bookIndex);
+                return BookCard(imageUrl: bestSellerBook.bookImage);
+              },
               options: CarouselOptions(
                 height: 1200 / MediaQuery.of(context).devicePixelRatio,
                 autoPlay: true,
--- lib/screens/widgets/book_card.dart
+++ lib/screens/widgets/book_card.dart
@@ -1,38 +1,32 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:mobile_app_project/dummy.dart';
-
-
 
 class BookCard extends StatelessWidget {
-  const BookCard({Key? key}) : super(key: key);
-
-
+  const BookCard({Key? key, this.imageUrl,}) : super(key: key);
+  final String? imageUrl;
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        width: 440/MediaQuery.of(context).devicePixelRatio,
-      padding: EdgeInsets.all(1),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-      ),
-      margin: EdgeInsets.all(8),
-      child: InkWell(
-        onTap: ()=>context.go('/home/book_details'),
-        child: ClipRRect(
+    return Container(
+        width: 440 / MediaQuery.of(context).devicePixelRatio,
+        padding: EdgeInsets.all(1),
+        decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
-
-           child: CachedNetworkImage(
-            fit:  BoxFit.fill,
-            imageUrl:Dummy.chooseRand(),
-            placeholder: (context, url) => Center(child: CircularProgressIndicator(color: Colors.red)),
-             errorWidget: (context, url, error) => Icon(Icons.error),
-             )
-             
         ),
-      )
-    );
+        margin: EdgeInsets.all(8),
+        child: InkWell(
+          onTap: () => {}, //context.go('/home/book_details'),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: (imageUrl != null) ? CachedNetworkImage(
+                fit: BoxFit.fill,
+                imageUrl: imageUrl ?? '',
+                placeholder: (context, url) => Center(
+                  child: CircularProgressIndicator(
+                      color: Theme.of(context).primaryColor),
+                ),
+                errorWidget: (context, url, error) => Icon(Icons.error_outline_rounded),
+              ) : Icon(Icons.error_outline_rounded)
+          ),
+        ));
   }
 }
