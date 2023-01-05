325cc1a4718dde8edf4300b9b9ccbb952027be07
Bilal Ghareeb <bilal.ghareeb@gmail.com>
2023-01-05 22:07:02 +0200

    Imports Fixing

--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile_app_project/business_logic/cubit/camera_cubit/camera_cubit.dart';
 import 'package:mobile_app_project/business_logic/cubit/google_books_cubit/google_books_cubit.dart';
@@ -10,10 +11,12 @@ import 'package:mobile_app_project/screens/widgets/display_picture.dart';
 import 'package:mobile_app_project/screens/widgets/take_picture.dart';
 import 'package:mobile_app_project/screens/widgets/text_recognizer_view.dart';
 import '../business_logic/bloc/app_status_bloc/app_status_bloc.dart';
+import '../business_logic/cubit/auth_cubit/auth_cubit.dart';
+import '../data/repository/auth_repository.dart';
+import '../screens/auth_screens/login/login_screen.dart';
+import '../screens/edit_profile/home/profile_screen.dart';
+import '../screens/home_screen/home/home_screen.dart';
 import '../screens/widgets/search_with_text.dart';
-import '../screens/widgets/display_picture.dart';
-import '../screens/widgets/take_picture.dart';
-import '../screens/widgets/text_recognizer_view.dart';
 ///
 
 /// To navigate use
