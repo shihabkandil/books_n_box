f84118a2ce574f850c0a4093887ca882894a7cda
mohamedAyman <modyayman2001@gmail.com>
2023-01-15 00:28:00 +0200

    GBS Api with Cubit continued

--- lib/data/web_services/google_web_services.dart
+++ lib/data/web_services/google_web_services.dart
@@ -2,17 +2,24 @@ import 'package:http/http.dart';
 import '../../utils/constants/strings.dart';
 import 'package:http/http.dart' as http;
 
-class GoogleWebService{
-
-  Future<Response> searchGoogleBooks({required String query})async{
-    try{
-      final uriRequest = Uri.parse(kGoogleBaseUrl+"?q=$query");
+class GoogleWebService {
+  Future<Response> searchGoogleBooks({required String query}) async {
+    try {
+      final uriRequest = Uri.parse(kGoogleBaseUrl + "?q=$query");
       Response response = await http.get(uriRequest);
       return response;
-    }
-    catch(e){
-      throw(e);
+    } catch (e) {
+      throw (e);
     }
   }
 
+  Future<Response> searchBooksByGenre({required String genre_name}) async {
+    try {
+      var url = Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': 'subject:' + genre_name});
+      Response response = await http.get(url);
+      return response;
+    } catch (e) {
+      throw (e);
+    }
+  }
 }
