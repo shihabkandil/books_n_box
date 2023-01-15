a52a32b0f653d06aa93c9109c97a4bcd0eb03257
Bilal Ghareeb <bilal.ghareeb@gmail.com>
2023-01-15 14:33:32 +0200

    Bookmark Button UI

--- lib/screens/widgets/book_card.dart
+++ lib/screens/widgets/book_card.dart
@@ -1,22 +1,31 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mobile_app_project/screens/widgets/bookmark_button.dart';
 
+import '../../business_logic/cubit/bookmarks_cubit/bookmarks_cubit.dart';
 import '../../data/models/google_books/google_book.dart';
 
 class BookCard extends StatelessWidget {
+  
+
   BookCard({
     Key? key,
     this.book,
-    this.imageUrl,
+    this.imageUrl, required this.hasBookmarkButton,
   }) : super(key: key);
+
   String? imageUrl;
   final GoogleBook? book;
+  final hasBookmarkButton;
+
   @override
   Widget build(BuildContext context) {
     if (book != null) {
       imageUrl = book!.volumeInfo?.imageLinks?.thumbnail;
     }
+
     return Container(
       width: 440 / MediaQuery.of(context).devicePixelRatio,
       padding: EdgeInsets.all(1),
@@ -26,26 +35,36 @@ class BookCard extends StatelessWidget {
       margin: EdgeInsets.all(8),
       child: InkWell(
         onTap: () => {context.go('/home/book_details')},
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: (imageUrl != null)
-              ? CachedNetworkImage(
-                  fit: BoxFit.fill,
-                  imageUrl: imageUrl!,
-                  placeholder: (context, url) => Center(
-                    child: CircularProgressIndicator(color: Theme.of(context).primaryColor),
-                  ),
-                  errorWidget: (context, url, error) => Icon(Icons.error_outline_rounded),
-                )
-              : Center(
-                child: SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: CircularProgressIndicator(
-                      color: Theme.of(context).primaryColor,
+        child: Stack(
+          fit: StackFit.expand,
+          clipBehavior: Clip.none,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: (imageUrl != null)
+                  ? CachedNetworkImage(
+                      fit: BoxFit.fill,
+                      imageUrl: imageUrl!,
+                      placeholder: (context, url) => Center(
+                        child: CircularProgressIndicator(
+                            color: Theme.of(context).primaryColor),
+                      ),
+                      errorWidget: (context, url, error) =>
+                          Icon(Icons.error_outline_rounded),
+                    )
+                  : Center(
+                      child: SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
                     ),
-                ),
-              ),
+            ),
+            if(hasBookmarkButton)
+              Positioned(left: 63, bottom: -9, child: BookmarkButton(onPressed: ()=>BlocProvider.of<BookmarksCubit>(context).recordBookMark(book),bookId: book?.id,)),
+          ],
         ),
       ),
     );
---
+++ lib/screens/widgets/bookmark_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_app_project/utils/enums/bookarks_enum.dart';
+
+import '../../business_logic/cubit/bookmarks_cubit/bookmarks_cubit.dart';
+
+class BookmarkButton extends StatelessWidget {
+  const BookmarkButton({super.key, required this.onPressed, required this.bookId});
+
+  final void Function() onPressed;
+  final String? bookId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.only(top: 10),
+      child: FittedBox(
+        child: Container(
+          height: 30,
+          width: 40,
+          decoration:
+              BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+          child: FittedBox(
+            child: BlocBuilder<BookmarksCubit , BookmarksState>(
+              builder: (context, state) {
+                var isBookmarked = BlocProvider.of<BookmarksCubit>(context).IsBookBookmarked(bookId);
+                print(isBookmarked);
+                if(state.status==BookmarkStatus.bookmarked && isBookmarked){
+                     return IconButton(
+                    onPressed: onPressed,
+                    icon:Icon(Icons.favorite),
+                    iconSize: 30,
+                    color: Colors.red,
+                  );
+                }else{
+                     return IconButton(
+                    onPressed: onPressed,
+                    icon:Icon(Icons.favorite_border),
+                    iconSize: 30,
+                    color: Colors.red,
+                  );
+                }
+                   
+              }
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
