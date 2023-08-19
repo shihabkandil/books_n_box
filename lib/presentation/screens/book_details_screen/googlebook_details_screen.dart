a9e59f4dd1f63c443fcc31b1e7d1610646361fa7
Shihab Kandil <jupitar.orbit01@gmail.com>
2023-08-19 21:17:12 +0300

    adding yaml workflow files

--- lib/presentation/screens/book_details_screen/googlebook_details_screen.dart
+++ lib/presentation/screens/book_details_screen/googlebook_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app_project/data/models/google_books/google_book.dart';
+
 import 'widgets/overview_header.dart';
 import 'widgets/overview_paragraph.dart';
 import 'widgets/rates_row.dart';
@@ -8,7 +9,7 @@ import 'widgets/screen_bar.dart';
 class GoogleBookDetailsScreen extends StatelessWidget {
   GoogleBook? googleBook;
   Map<String, dynamic>? allBook;
-  GoogleBookDetailsScreen({Key? key, this.googleBook,  this.allBook})
+  GoogleBookDetailsScreen({Key? key, this.googleBook, this.allBook})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -39,28 +40,6 @@ class GoogleBookDetailsScreen extends StatelessWidget {
                     pagecount: pagecount),
                 OverViewHeader(authors: authors),
                 OverViewParagraph(bookDescription: bookDescription),
-                BlocBuilder<ReviewsCubit,ReviewsState>(
-                  builder: (context,state) {
-                    if(state.status == ReviewsStatus.submittingReview){
-                      return SizedBox(
-                          height: MediaQuery.of(context).size.width / 100 * 4,
-                          width: MediaQuery.of(context).size.width / 100 * 4,
-                          child: Center(child: CircularProgressIndicator()));
-                    }
-                    else if(state.status == ReviewsStatus.reviewSaved){
-                      return Container(
-                        margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.height / 100 * 5,vertical: MediaQuery.of(context).size.height / 100 * 2),
-                        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.height / 100 * 5,vertical: MediaQuery.of(context).size.height / 100 * 2),
-                        decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: HeaderText(text: AppLocalizations.of(context)!.review_submitted,color: Colors.white),
-                      );
-                    }
-                    return ReviewSwitcher();
-                  }
-                ),
               ],
             ),
           ),
