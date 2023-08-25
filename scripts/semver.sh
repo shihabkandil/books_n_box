93d97fd85ee93b9ddc04a97a6e895139643b6fa3
Shihab Kandil <79358747+shihabkandil@users.noreply.github.com>
2023-08-25 12:55:54 +0300

    Update semver.sh

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -11,11 +11,11 @@ else
   nextVersion="$2"
 fi
 
-LAST_UAT_BUILD_NUMBER=`echo $lastVersion | sed -E "s/$RE/\4/"`
+LAST_PATCH_NUMBER=`echo $lastVersion | sed -E "s/$RE/\3/"`
 
 NEXT_VERSION_MAJOR=`echo $nextVersion | sed -E "s/$RE/\1/"`
 NEXT_VERSION_MINOR=`echo $nextVersion | sed -E "s/$RE/\2/"`
 NEXT_VERSION_PATCH=`echo $nextVersion | sed -E "s/$RE/\3/"`
 
-((LAST_UAT_BUILD_NUMBER++))
-echo "$NEXT_VERSION_MAJOR.$NEXT_VERSION_MINOR.$NEXT_VERSION_PATCH+$LAST_UAT_BUILD_NUMBER"
+((LAST_PATCH_NUMBER++))
+echo "$NEXT_VERSION_MAJOR.$NEXT_VERSION_MINOR.$NEXT_VERSION_PATCH"
